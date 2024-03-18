9288b745089294d416a9ed9c75d866b3a68638fa
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2024-03-17 20:46:39 -0400

    UPDATE: Added necessary logic to split the results from the Database to show in HTML.
    Using the ID's of the DIV elements, assign the respective DIV id.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -6,7 +6,15 @@ function getQueryResults() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             // The results of the query are caught here and are posted as an alert.
-            alert(xhr.responseText);
+            queryResults = xhr.response
+            // Creates a list of all the results from the Query
+            queryResults = queryResults.toString().split("\n")
+            // Inserting the data the first resutl to HTML
+            firstQuery = queryResults[0].toString().split(",")
+            document.getElementById("recipeName001").innerHTML = "Recipe Name: " + firstQuery[1].toString()
+            document.getElementById("image001").src = firstQuery[2].toString()
+            document.getElementById("recipeRating001").innerHTML = "Rating: " + firstQuery[3].toString()
+            document.getElementById("recipeIngredients001").innerHTML = "Ingredients: " + firstQuery[4].toString()
         }
     };
     xhr.send("");
