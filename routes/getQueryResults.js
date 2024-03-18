c72b9a31e2d8f110731c8a00fa31a0a552826fca
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2024-03-18 14:28:13 -0400

    UPDATE: Correct database added.

--- routes/getQueryResults.js
+++ routes/getQueryResults.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'ENTER YOUR PASSWORD FOR SQL DATABASE HERE',
-    database: 'ADVENTURER'
+    database: 'Kitchen'
 });
 
 router.post('/', (req, res) => {
